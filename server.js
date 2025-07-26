9dbbb1dec02fee19840518298f326c5b93e2f8d4
Bob Holland <30559674+SmagicJones@users.noreply.github.com>
2025-07-26 07:57:43 +0100

    back track a little

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const corsOptions = {
   optionsSuccessStatus: 200, // For legacy browser support
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 app.post("/aistream", async (req, res) => {
